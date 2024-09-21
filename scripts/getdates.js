06cebb9a7925eab0692ebba0f373d57e08368977
nwanodicv <victornwanodi@gmail.com>
2024-09-21 16:35:12 +0100

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,20 +1,3 @@
-
-/*/ select the DOM elements for output
-const full = document.querySelector("#last-modification");
-
-
-// use the date object
-const today = new Date();
-
-full.innerHTML = `Last Modification <span class="highlight">${new Intl.DateTimeFormat(
-	"en-US",
-	{
-		lastmodified: "full",
-	}
-).format(today)}`
-
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear().getFullTime()}`;*/
-
 let lastModified = document.querySelector('#last-modification');
 
 lastModified.innerHTML = ""
