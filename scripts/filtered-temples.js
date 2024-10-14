b9ff0301f06df78c0be6e91b5f852a5ec0873dfd
nwanodicv <victornwanodi@gmail.com>
2024-10-14 06:20:11 +0100

    update

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,63 +1,71 @@
 const temples = [
     {
+      year: "2005",
       templeName: "Aba Nigeria",
       location: "Aba, Nigeria",
       dedicated: "2005, August, 7",
-      area: 11500,
+      area: "11500",
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
     },
     {
+      year: "1888",
       templeName: "Manti Utah",
       location: "Manti, Utah, United States",
       dedicated: "1888, May, 21",
-      area: 74792,
+      area: "74792",
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
     },
     {
+      year: "2015",
       templeName: "Payson Utah",
       location: "Payson, Utah, United States",
       dedicated: "2015, June, 7",
-      area: 96630,
+      area: "96630",
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
     },
     {
+      year: "2020",
       templeName: "Yigo Guam",
       location: "Yigo, Guam",
       dedicated: "2020, May, 2",
-      area: 6861,
+      area: "6861",
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
     },
     {
+      year: "1974",
       templeName: "Washington D.C.",
       location: "Kensington, Maryland, United States",
       dedicated: "1974, November, 19",
-      area: 156558,
+      area: "156558",
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
     },
     {
+      year: "1986",
       templeName: "Lima Perú",
       location: "Lima, Perú",
       dedicated: "1986, January, 10",
-      area: 9600,
+      area: "9600",
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
     },
     {
+      year: "1983",
       templeName: "Mexico City Mexico",
       location: "Mexico City, Mexico",
       dedicated: "1983, December, 2",
-      area: 116642,
+      area: "116642",
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
     },
     // Add more temple objects here...
     {
-        templeName: "Atlanta Georgia",
+      year: "1983",
+      templeName: "Atlanta Georgia",
       location: "Sandy Springs, Georgia",
       dedication: "June 1, 1983",
       area: "30328",
@@ -66,6 +74,7 @@ const temples = [
     },
 
     {
+      year: "2004",
       templeName: "Accra Ghana",
       location: "Accra Ghana",
       dedication: " 11 January 2004",
@@ -75,6 +84,7 @@ const temples = [
     },
 
     {
+      year: "1985",
       templeName: "South Africa",
       location: "Parktown, Johannesburg",
       dedication: "24 August 1985",
@@ -84,6 +94,7 @@ const temples = [
   },
   
   {
+    year: "2000",
     templeName: "Melbourne Australia",
     location: "76 Cathies, Wantirna South",
     dedication: "June 16, 2000",
@@ -93,14 +104,14 @@ const temples = [
 },
 
 {
+  year: "2001",
   templeName: "Perth Australia",
   location: " 163 Wordsworth, Yokine, Australia",
   dedication: " May 20, 2001",
   area: " 10,700",
   imageUrl: 
   "https://churchofjesuschristtemples.org/assets/img/temples/perth-australia-temple/perth-australia-temple-34994-main.jpg"
-}
-  ];
+}];
 
 let albumHTML = '';
 
@@ -113,7 +124,7 @@ temples.forEach((temple) => {
             <h5>location:  ${temple.location}</h5>
             <h5>dedicated:  ${temple.dedicated}</h5>
             <h5>area:  ${temple.area}</h5>
-            <img src="${temple.imageUrl}">
+            <img src="${temple.imageUrl}" alt="Aba Nigeria Temple" loading="lazy">
         </figure>
          <figure>
                     
@@ -128,6 +139,87 @@ temples.forEach((temple) => {
 document.querySelector('.main-album-container').innerHTML = albumHTML;
 
 
+function oldTemple() {
+  temples.forEach((temple) => {
+      addEventListener('click', () => {
+      let oldTemples = "";
+      if (temple.year < "1900") {
+        oldTemples = temple.year;
+      }else
+      {
+        return 
+      }
+      console.log(oldTemples)
+      document.querySelector('.nav-links-output') = oldTemples
+      //oldTemples = temples.filter(temple =>!temple.location.includes(temple.year));
+    })
+  })
+}
+
+
+function newTemple() {
+  temples.forEach((temple) => {
+      addEventListener('click', () => {
+      let newTemples = "";
+      if (temple.year > "1900" || temple.year <= "2000") {
+        newTemples = temple.year;
+      }else
+      {
+        return temple
+      }
+      console.log(newTemples)
+      document.querySelector('.nav-links-output') = newTemples
+      //oldTemples = temples.filter(temple =>!temple.location.includes(temple.year));
+    })
+  })
+};
+
+
+function largeTemple() {
+  temples.forEach((temple) => {
+      addEventListener('click', () => {
+      let largeTemples = "";
+      if (temple.area > "90000") {
+        largeTemples = temple.area;
+      }else
+      {
+        return temple
+      }
+      console.log(largeTemples)
+      document.querySelector('.nav-links-output') = largeTemples
+      //oldTemples = temples.filter(temple =>!temple.location.includes(temple.year))
+    })
+  })
+};
+
+function smallTemple() {
+  temples.forEach((temple) => {
+      addEventListener('click', () => {
+      let smallTemples = "";
+      if (temple.area < "10000") {
+        smallTemples = temple.area;
+      }else
+      {
+        return
+      }
+      console.log(smallTemples)
+      //oldTemples = temples.filter(temple =>!temple.location.includes(temple.year));
+      document.querySelector('.nav-links-output') = smallTemples
+    })
+  })
+}
+
+//const navLinksOutput = document.querySelector('.js-home-page');
+//
+//navLinksOutput.addEventListener('click', () => {
+//  oldTemple();
+//  newTemple();
+//  largeTemple();
+//  smallTemple();
+//});
+
+
+// page datetime last modification
 let lastModified = document.querySelector('#last-modification');
 
 lastModified.innerHTML = ""
