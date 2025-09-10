245481f01a2cadc6df0a28e03b738029c4fc0c9e
nwanodicv <victornwanodi@gmail.com>
2025-09-10 21:28:18 +0100

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,19 @@
+const menuButton = document.querySelector("#menu-button");
+const navMenu = document.querySelector(".nav-bar-container");
+
+menuButton.addEventListener("click", () => {
+  navMenu.classList.toggle("open");
+
+  // Toggle button symbol
+  if (navMenu.classList.contains("open")) {
+    menuButton.textContent = "✖"; // Close icon
+  } else {
+    menuButton.textContent = "☰"; // Hamburger icon
+  }
+});
+
+
+
 let lastModified = document.querySelector('#last-modification');
 
 lastModified.innerHTML = ""
@@ -10,9 +26,9 @@ let nLastModif = Date.parse(document.lastModified);
 lastModified.innerHTML = `<span class="highlight">${new Intl.DateTimeFormat(
 	"en-US",
 	{
-		nLastModif: "full",
+		dateStyle: "full", timeStyle: "long",
 	}
 ).format(nLastModif)
 }`;
 
-lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
+//lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
