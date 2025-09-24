f327ba6344e1cbbbda06070e4c68b5fea7db6fca
nwanodicv <victornwanodi@gmail.com>
2025-09-24 03:05:26 +0100

    update

--- scripts/place.js
+++ scripts/place.js
@@ -1,10 +1,20 @@
 // Last Modified Date
 let lastModified = document.querySelector('#last-modification');
+
+lastModified.innerHTML = ""
+
+
+// returns: Tuesday, December 16, 2017 11:09:42
+let oLastModif = new Date(document.lastModified);
 let nLastModif = Date.parse(document.lastModified);
-lastModified.innerHTML = `Last Modified: ${new Intl.DateTimeFormat(
-  "en-US",
-  { dateStyle: "full", timeStyle: "long" }
-).format(nLastModif)}`;
+
+lastModified.innerHTML = `<span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full", timeStyle: "long",
+	}
+).format(nLastModif)
+}`;
 
 // Wind Chill Calculation
 function calculateWindChill(t, s) {
